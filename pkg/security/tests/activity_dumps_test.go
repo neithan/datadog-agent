bd2163cc0ea522526bee82973cec3a8aaee8295c
gui774ume <guillaume.fournier@datadoghq.com>
2022-07-03 00:33:10 +0200

    add tests

--- pkg/security/tests/activity_dumps_test.go
+++ pkg/security/tests/activity_dumps_test.go
@@ -17,6 +17,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/DataDog/datadog-agent/pkg/security/secl/model"
 	"github.com/DataDog/datadog-agent/pkg/security/secl/rules"
 )
 
@@ -93,7 +94,7 @@ func TestActivityDumps(t *testing.T) {
 				}
 				node := ad.FindFirstMatchingNode("syscall_tester")
 				if node == nil {
-					t.Fatal("Node not found on activity dump")
+					t.Fatal("Node not found in activity dump")
 				}
 				for _, s := range node.Sockets {
 					if s.Family == "AF_INET" && s.Bind.Port == 4242 && s.Bind.IP == "0.0.0.0" {
@@ -102,7 +103,7 @@ func TestActivityDumps(t *testing.T) {
 					}
 				}
 				if msgpOK == false {
-					t.Error("Bound socket not found on activity dump")
+					t.Error("Bound socket not found in activity dump")
 				}
 
 			default:
@@ -111,7 +112,7 @@ func TestActivityDumps(t *testing.T) {
 
 		}
 		if jsonOK == false || msgpOK == false {
-			t.Fatal("Some files are missing, got:", outputFiles)
+			t.Fatal("Some data is missing, got:", outputFiles)
 		}
 	})
 
@@ -160,7 +161,7 @@ func TestActivityDumps(t *testing.T) {
 				}
 				node := ad.FindFirstMatchingNode("testsuite")
 				if node == nil {
-					t.Fatal("Node not found on activity dump")
+					t.Fatal("Node not found in activity dump")
 				}
 				for name := range node.DNSNames {
 					if name == "foo.bar" {
@@ -169,7 +170,7 @@ func TestActivityDumps(t *testing.T) {
 					}
 				}
 				if msgpOK == false {
-					t.Error("DNS request not found on activity dump")
+					t.Error("DNS request not found in activity dump")
 				}
 
 			default:
@@ -178,7 +179,88 @@ func TestActivityDumps(t *testing.T) {
 
 		}
 		if jsonOK == false || msgpOK == false {
-			t.Fatal("Some files are missing, got:", outputFiles)
+			t.Fatal("Some data is missing, got:", outputFiles)
+		}
+	})
+
+	test.Run(t, "activity-dump-comm-syscalls", func(t *testing.T, kind wrapperType,
+		cmdFunc func(cmd string, args []string, envs []string) *exec.Cmd) {
+
+		outputFiles, err := test.StartActivityDumpComm(t, "syscall_tester", outputDir, []string{"json", "msgp"})
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		args := []string{"bind", "AF_INET", "any", "tcp"}
+		envs := []string{}
+		cmd := cmdFunc(syscallTester, args, envs)
+		if out, err := cmd.CombinedOutput(); err != nil {
+			t.Fatal(fmt.Errorf("%s: %w", out, err))
+		}
+
+		time.Sleep(1 * time.Second) // a quick sleep to let events to be added to the dump
+
+		err = test.StopActivityDumpComm(t, "syscall_tester")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		jsonOK := false
+		msgpOK := false
+		for _, f := range outputFiles {
+			ext := filepath.Ext(f)
+			switch ext {
+			case ".json":
+				if jsonOK == true {
+					t.Fatal("Got more than one JSON file:", outputFiles)
+				}
+				content, err := os.ReadFile(f)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if !validateActivityDumpSchema(t, string(content)) {
+					t.Error(string(content))
+				}
+				jsonOK = true
+
+			case ".msgp":
+				if msgpOK == true {
+					t.Fatal("Got more than one MSGP file:", outputFiles)
+				}
+				ad, err := test.DecodeMSPActivityDump(t, f)
+				if err != nil {
+					t.Fatal(err)
+				}
+				node := ad.FindFirstMatchingNode("syscall_tester")
+				if node == nil {
+					t.Fatal("Node not found in activity dump")
+				}
+				var exitOK, execveOK bool
+				for _, s := range node.Syscalls {
+					if s == int(model.SysExit) || s == int(model.SysExitGroup) {
+						exitOK = true
+					}
+					if s == int(model.SysExecve) || s == int(model.SysExecveat) {
+						execveOK = true
+					}
+				}
+				if !exitOK {
+					t.Errorf("exit syscall not found in activity dump: %+v", node.Syscalls)
+				} else {
+					if !execveOK {
+						t.Errorf("execve syscall not found in activity dump: %+v", node.Syscalls)
+					} else {
+						msgpOK = true
+					}
+				}
+
+			default:
+				t.Fatal("Unexpected output file")
+			}
+
+		}
+		if jsonOK == false || msgpOK == false {
+			t.Fatal("Some data is missing, got:", outputFiles)
 		}
 	})
 }
